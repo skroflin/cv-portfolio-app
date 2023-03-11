223ac2ce6b5fdef5c355848e4b2aa6293f1fa8d8
skroflin <sven.kroflin@gmail.com>
2023-03-11 15:02:53 +0100

    update na Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,6 +9,10 @@ function Intro() {
             <p className="text-base md:text-xl mb-3 font-medium">
                 Student <a href="https://www.ffos.unios.hr/" target="_blank" className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-cyan-600" rel="noreffer noopener">@Filozofski fakultet Osijek</a>
             </p>
+            <p className="text-sm max-w-xl mb-6">
+                My name is Sven Kroflin. I am 21 years old and I am a student at the Faculty of Humanities and Social Sciences. I am currently pursuing a undergraduate in Information and Library Sciences. 
+                I am currently aspiring to one day work in the IT industry, preferably as a Frontend developer.
+            </p>
         </div>
     )
 }
