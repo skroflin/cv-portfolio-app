2a731cc43759697328a65556981aa2a9d5a09983
skroflin <sven.kroflin@gmail.com>
2023-03-11 14:56:47 +0100

    dodan light i dark mode

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-10">
-            <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">
+            <h1 className="text-4xl md:text-7x1 dark:text-white mb-1 md:mb-3 font-bold">
                 Sven Kroflin
             </h1>
             <p className="text-base md:text-xl mb-3 font-medium">
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,7 +5,8 @@ function Title({ children, id }) {
         <h1
             id={id && id}
             className="text-2xl font-bold underline
-            underline-offset-8 decoration-4 mb-5 text-stone-900"
+            underline-offset-8 decoration-4 mb-5 text-stone-900
+            dark:text-white"
         >
             {children}
         </h1>
